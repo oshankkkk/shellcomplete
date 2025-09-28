be9725a5160789a82779f8a8526a8cd4ef4a6dab
Oshan Kodagoda <oshankodagoda200667@gmail.com>
2025-09-28 10:02:26 +0530

    get shell history and display on the terminal with half baked key input implementation

--- main.go
+++ main.go
@@ -3,31 +3,49 @@ package main
 import (
 	"fmt"
 	tea "github.com/charmbracelet/bubbletea"
+
+	
 )
-type cup struct{}
+type cup struct{
+	historylist []string
+	index int
+}
 
-func (c cup) Init() (tea.Cmd){
-return nil}
 
-func (c cup) Update(msg tea.Msg)(tea.Model, tea.Cmd){
+func (c *cup) Init() (tea.Cmd){
+	bashistoryfile,err:=getbashhistory();if err!=nil{
+		fmt.Println(err)
+	}
+	c.historylist =readbashhistory(bashistoryfile)
+	return nil
+}
+
+func (c *cup) Update(msg tea.Msg)(tea.Model, tea.Cmd){
 	if keymsg,okay:=msg.(tea.KeyMsg);okay{
 		if  keymsg.String()=="q"{
 			return c,tea.Quit
 		}
+		c.index++
+
 	}
-	
 	return c,nil
 }
 
-func (c cup) View() string{
-return "hi" }
+func (c *cup) View() string{
+  if len(c.historylist) == 0 {
+        return "No history found"
+    }
+    return fmt.Sprintf("%s", c.historylist[c.index])
+ }
+
+
 
 func main(){
 	fmt.Println("hi")
-	app:=tea.NewProgram(cup{})
+	app:=tea.NewProgram(&cup{})
 	if _,err:=app.Run();err!=nil{
 		fmt.Println("kela una", err)
 	}
-	
+
 }
 
