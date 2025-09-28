3dc91733519a698f051ad4aead93fccf2063bff7
Oshan Kodagoda <oshankodagoda200667@gmail.com>
2025-09-28 23:28:54 +0530

    day 1 pinish

--- chronicals.md
+++ chronicals.md
@@ -1,13 +1,13 @@
-# How bubble tea works 
+# How things i notice as i Go
+
+## How bubble tea works 
 > Bubble tea is a Go framework like react but for the terminal
 > There is a event loop that runs and thats our app
 > There are 3 functions int, update and view which runs on the startup, when an event happen and the state of the app
 > There is a struct called model in go, thats our app essentialy, it can have our app data like state variables and what not
 
-# How things i notice bout go
 ### Werid UTF shenanagans
 ### Structs and Methods, not classes
 > Go uses Structs and Methods like a method is a func that accepts arguments struct.
-
 
 
--- main.go
+++ main.go
@@ -6,13 +6,15 @@ import (
 
 	
 )
-type cup struct{
+type shellcomplete struct{
 	historylist []string
 	index int
+	sortedhistorylist []string
 }
 
 
-func (c *cup) Init() (tea.Cmd){
+
+func (c *shellcomplete) Init() (tea.Cmd){
 	bashistoryfile,err:=getbashhistory();if err!=nil{
 		fmt.Println(err)
 	}
@@ -20,18 +22,18 @@ func (c *cup) Init() (tea.Cmd){
 	return nil
 }
 
-func (c *cup) Update(msg tea.Msg)(tea.Model, tea.Cmd){
+func (c *shellcomplete) Update(msg tea.Msg)(tea.Model, tea.Cmd){
 	if keymsg,okay:=msg.(tea.KeyMsg);okay{
 		if  keymsg.String()=="q"{
 			return c,tea.Quit
 		}
-		c.index++
+		
 
 	}
 	return c,nil
 }
 
-func (c *cup) View() string{
+func (c *shellcomplete) View() string{
   if len(c.historylist) == 0 {
         return "No history found"
     }
@@ -42,10 +44,11 @@ func (c *cup) View() string{
 
 func main(){
 	fmt.Println("hi")
-	app:=tea.NewProgram(&cup{})
+	app:=tea.NewProgram(&shellcomplete{})
 	if _,err:=app.Run();err!=nil{
 		fmt.Println("kela una", err)
 	}
+
 
 }
 
