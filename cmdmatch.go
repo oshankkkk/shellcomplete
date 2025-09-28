60e98e5998e1ac85f69fb0a9fd399697207875b0
Oshan Kodagoda <oshankodagoda200667@gmail.com>
2025-09-28 23:15:13 +0530

    add edit distance algo

---
+++ cmdmatch.go
@@ -0,0 +1,49 @@
+package main
+
+func min(a, b, c int) int {
+	if a < b {
+		if a < c {
+			return a
+		}
+		return c
+	}
+	if b < c {
+		return b
+	}
+	return c
+}
+
+func editDistance(s1, s2 string) int {
+	m, n := len(s1), len(s2)
+
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+
+	for i := 0; i <= m; i++ {
+		dp[i][0] = i
+	}
+	for j := 0; j <= n; j++ {
+		dp[0][j] = j
+	}
+
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if s1[i-1] == s2[j-1] {
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				dp[i][j] = 1 + min(
+					dp[i-1][j],    // deletion
+					dp[i][j-1],    // insertion
+					dp[i-1][j-1],  // substitution
+				)
+			}
+		}
+	}
+
+	return dp[m][n]
+}
+
+
+
