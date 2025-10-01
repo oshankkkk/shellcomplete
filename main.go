2d71c81b59f0ef0a56edfb75d86ad05f7b3debbb
Oshan Kodagoda <oshankodagoda200667@gmail.com>
2025-10-01 23:43:13 +0530

    add bubbletea suggesion box

--- main.go
+++ main.go
@@ -1,50 +1,13 @@
 package main
 
-import (
-	"fmt"
+import ( 
+
 	tea "github.com/charmbracelet/bubbletea"
-
-	
-)
-type shellcomplete struct{
-	historylist []string
-	index int
-	sortedhistorylist []string
-}
-
-
-
-func (c *shellcomplete) Init() (tea.Cmd){
-	bashistoryfile,err:=getbashhistory();if err!=nil{
-		fmt.Println(err)
-	}
-	c.historylist =readbashhistory(bashistoryfile)
-	return nil
-}
-
-func (c *shellcomplete) Update(msg tea.Msg)(tea.Model, tea.Cmd){
-	if keymsg,okay:=msg.(tea.KeyMsg);okay{
-		if  keymsg.String()=="q"{
-			return c,tea.Quit
-		}
-		
-
-	}
-	return c,nil
-}
-
-func (c *shellcomplete) View() string{
-  if len(c.historylist) == 0 {
-        return "No history found"
-    }
-    return fmt.Sprintf("%s", c.historylist[c.index])
- }
-
-
+	"fmt")
 
 func main(){
 	fmt.Println("hi")
-	app:=tea.NewProgram(&shellcomplete{})
+	app:=tea.NewProgram(& popup{})
 	if _,err:=app.Run();err!=nil{
 		fmt.Println("kela una", err)
 	}
