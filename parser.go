bf705b8e90e98bfe2df87ccea31c6b51059c1b42
oshankkk <oshankodagoda200667@gmail.com>
2025-10-26 00:29:49 +0530

    add scanner to input reader line by line reading i think

--- parser.go
+++ parser.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
 
 //input filepath is given
 func inputReader(filepath string){
+
 fmt.Println("filepath",filepath)
-file,_:=os.ReadFile(filepath)
-fmt.Println(string(file))
+// this loads the whole file into memory
+//file,_:=os.ReadFile(filepath)
+//fmt.Println(string(file))
+file,_:=os.Open(filepath)
+defer file.Close()
+scanner:=bufio.NewScanner(file)
+for scanner.Scan(){
+	fmt.Println(scanner.Text())
+}
 
 }
 func lexer(){}
